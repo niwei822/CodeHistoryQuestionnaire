e46bbbc85ab69c83fec51f8e2a831b47ba033c6e
niwei822 <cecily.li.822@gmail.com>
2022-09-24 23:09:44 -0700

    add more views

--- CodeHistory/Views/GameView.swift
+++ CodeHistory/Views/GameView.swift
@@ -7,32 +7,32 @@
 
 import SwiftUI
 
-struct QustionView: View {
+struct GameView: View {
     let question = Question(
             questionText: "What was the first computer bug?",
             possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
             correctAnswerIndex: 2)
     
-   @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = GameColor.main
    
     var body: some View {
         ZStack {
             mainColor.ignoresSafeArea()
             VStack {
-                Text("1 / 10")
+              VStack  {Text("1 / 10")
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
                 Text(question.questionText)
                     .font(.largeTitle)
                     .bold()
-                    .multilineTextAlignment(.leading)
+                    .multilineTextAlignment(.leading)}
                 Spacer()
                 HStack {
                     ForEach(0..<question.possibleAnswers.count) { answerIndex in
                         Button(action: {
                             print("Tapped on opthion with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                            mainColor = answerIndex == question.correctAnswerIndex ? GameColor.correctGuess : GameColor.incorrectGuess
                     }) {
                        ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                     }
@@ -46,6 +46,6 @@ struct QustionView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        QustionView()
+        GameView()
     }
 }
---
+++ CodeHistory/Views/WelcomeView.swift
@@ -0,0 +1,40 @@
+//
+//  WelcomeView.swift
+//  CodeHistory
+//
+//  Created by cecily li on 9/24/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                GameColor.main.ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Select the correct answers to the following questions.")
+                            .font(.largeTitle)
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                    }
+                    Spacer()
+                    NavigationLink(destination: GameView(),
+                                   label: {
+                        BottomTextView(str: "Okay let's go!")
+                    })
+                }
+                .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
